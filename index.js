95941a0fa3678a3147f5f42d5fe3cbc5a242ad2e
Matt Lyons <matt@matt-lyons.net>
2016-10-14 18:35:24 -0700

    Implement Commander Support
    Should make adding command line flags much easier.
    Closes #1

--- index.js
+++ index.js
@@ -1,34 +1,32 @@
 #! /usr/bin/env node
+const args = require("commander")
 const copyPaste = require("copy-paste")
 const Tesseract = require("tesseract.js")
 const Progress = require("progress")
-let args = process.argv.slice(2)
-let lang = "eng"
-const usageTmpl = `Command line utility that copies image text to clipboard
+const PkgJson = require("./package.json")
 
-Usage: imgclip PATH [options...]
+//Used in help
+args.usage("PATH [options]")
+args.description(PkgJson.description)
 
-Options:
+//Arguments (Order determines order shown in help)
+args.option("-l, --lang [language]", "Language of the text in the image.\n\t"+
+	"Full language list can be found here: "+
+	"https://github.com/naptha/tesseract.js/blob/master/docs/tesseract_lang_list.md")
+args.version(PkgJson.version)
 
-	--lang LANGUAGE    language of the text in the image. 
+args.parse(process.argv)
 
-Full language list can be found here: https://github.com/naptha/tesseract.js/blob/master/docs/tesseract_lang_list.md`
-
-function printUsage() {
-	console.log(usageTmpl)
+if(!args.args.length || args.args[0] === undefined){
+	console.error("  No Path Specified")
+	args.help()
+	return
 }
 
-if (args.length == 0 || args.length > 3) {
-	printUsage()
-} else {
-	for (let i = 0; i < args.length - 1; i++) {
-		if (args[i] == "--lang") {
-			lang = args[i + 1]
-			args.splice(i, 2)
-		}
-	}
-	recognize(args[0], lang)
-}
+if(args.lang === undefined)
+	args.lang = "eng"
+
+recognize(args.args[0], args.lang)
 
 function recognize(imagePath, lang) {
 	const bar = new Progress("recognizing [:bar] :percent :elapseds", {total: 100})
